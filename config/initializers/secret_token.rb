af002c3c7d4baaa799d599d5b33a8ca82c79ebb4
E-Xor <e-xormail@tut.by>
2011-06-17 14:32:39 +0300

    Switch to Rails 3, part 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloWorldTests::Application.config.secret_token = '2847e559c70f3e62e89f5a6a062ced553d8a1a028d09f1e7d5328a04a21bf3990447143d50311f2c5c690ade7e6c5aae5d0a36f8ee946f29adf46eb871b197e1'
