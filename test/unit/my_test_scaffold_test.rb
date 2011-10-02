66f5f01198eeaaa6d1522a61f31e9916ffcfbec8
E-Xor <e-xormail@tut.by>
2011-10-02 21:50:08 +0300

    Controller and model tests

--- test/unit/my_test_scaffold_test.rb
+++ test/unit/my_test_scaffold_test.rb
@@ -11,4 +11,44 @@ class MyTestScaffoldTest < ActiveSupport::TestCase
     assert my_test_scaffold.errors[:image_url].any?
   end
 
+  def new_my_test_scaffold(price)
+
+    my_test_scaffold = MyTestScaffold.new(
+      :title => 'Test Book',
+      :description => 'Description of Test Book',
+      :image_url => 'images/test.png',
+      :price => price
+    )
+
+  end
+
+  test "price must be positive" do
+
+    nmts = new_my_test_scaffold(-1)
+    assert nmts.invalid?
+    # Asserted message should ge exactly like validation message, that's what we check here
+    assert_equal 'must be greater than or equal to 0.01', nmts.errors[:price].join('; ')
+
+    nmts = new_my_test_scaffold(0)
+    assert nmts.invalid?
+    assert_equal 'must be greater than or equal to 0.01', nmts.errors[:price].join('; ')
+
+    assert new_my_test_scaffold(1).valid?
+
+  end
+
+  test "title must be unique" do
+
+    # First instance is in the fixture
+    my_test_scaffold = MyTestScaffold.new(
+      :title => 'Title Five',
+      :description => 'Description of title 5',
+      :image_url => '/images/image5.png',
+      :price => 50.99
+    )
+    assert !my_test_scaffold.save
+    assert_equal 'has already been taken. Should be unique.', my_test_scaffold.errors[:title].join('; ')
+
+  end
+
 end
