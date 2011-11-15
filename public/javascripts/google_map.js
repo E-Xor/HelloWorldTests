39e5941c52412a0819c397e9e17bc76afae8cdf2
E-Xor <e-xormail@tut.by>
2011-11-15 06:16:17 -0800

    Google Maps. Add Directions.

--- public/javascripts/google_map.js
+++ public/javascripts/google_map.js
@@ -5,7 +5,7 @@ geocoder = new google.maps.Geocoder();
     var myOptions = {
       zoom: 4,
       center: new google.maps.LatLng(-34.397, 150.644),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeId: google.maps.MapTypeId.HYBRID,
       // Add some controls
       panControl: true,
       zoomControl: true,
@@ -20,7 +20,7 @@ geocoder = new google.maps.Geocoder();
   google.maps.event.addDomListener(window, 'load', initialize);
 
   function reLocate() {
-    address = document.getElementById("map_address").value;
+    address = document.getElementById("from_address").value;
     if (address != '') {
       if (geocoder) {
         geocoder.geocode( { 'address': address}, function(results, status) {
@@ -33,3 +33,55 @@ geocoder = new google.maps.Geocoder();
        }
     }
   }
+
+function showDirectionsViaEnter() {
+  evt = event;
+  var charCode = (evt.charCode) ? evt.charCode : ((evt.which) ? evt.which : evt.keyCode);
+  if (charCode == 13) {
+    showDirections();
+  }
+  return true;
+}
+
+function showDirections () {
+  var location_to = new google.maps.LatLng(-34.397, 150.644);
+  var location = new google.maps.LatLng(-33.397, 151.644);
+  alert('Locations: ' + location + location_to);
+
+  directionsDisplay = new google.maps.DirectionsRenderer(
+//{
+//   suppressMarkers: true,
+//      suppressInfoWindows: true
+//      }
+  );
+  directionsDisplay.setMap(map);
+
+
+  var request = {
+    origin:location,
+    destination:location_to,
+    travelMode: google.maps.DirectionsTravelMode.DRIVING
+  };
+
+  directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status)
+  { alert('directionsService.route');
+    if (status == google.maps.DirectionsStatus.OK)
+    { alert('Status OK');
+      directionsDisplay.setDirections(response);
+      distance = "The distance between the two points on the chosen route is: "+response.routes[0].legs[0].distance.text;
+      distance += "The aproximative driving time is: "+response.routes[0].legs[0].duration.text;
+      //document.getElementById("distance_road").innerHTML = distance;
+      alert ('Distance: ' + distance);
+    }
+  });
+}
+
+function reLocateViaEnter() {
+  evt = event;
+  var charCode = (evt.charCode) ? evt.charCode : ((evt.which) ? evt.which : evt.keyCode);
+  if (charCode == 13) {
+    reLocate();
+  }
+  return true;
+}
