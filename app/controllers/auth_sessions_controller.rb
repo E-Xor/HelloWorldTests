1821983dd36cc6e95be0975345b22227fc92d4fc
E-Xor <e-xormail@tut.by>
2012-05-18 20:46:45 +0300

    Redirect to create user if there is no users in DB

--- app/controllers/auth_sessions_controller.rb
+++ app/controllers/auth_sessions_controller.rb
@@ -1,6 +1,5 @@
 class AuthSessionsController < ApplicationController
 layout 'my_book_store'
-  skip_before_filter :authorize
 
   def new
   end
@@ -10,6 +9,8 @@ layout 'my_book_store'
       session[:auth_user_id] = user.id
       session[:auth_user_name] = params[:user_name]
       redirect_to admin_url
+    elsif AuthUser.count.zero?
+      redirect_to url_for(:controller => 'auth_users', :action => 'new'), :notice => "You're redirected here because there is no users in database. Please create one to make everything more secure and then sign in."
     else
       redirect_to auth_login_url, :alert => "Invalid user or password"
     end
