ba42c7f03b783b3a1feb511feb66a1a6eea74215
E-Xor <e-xormail@tut.by>
2012-06-07 18:44:11 +0300

    Finish with Spec tests

--- spec/controllers/my_store_controller_spec.rb
+++ spec/controllers/my_store_controller_spec.rb
@@ -6,6 +6,8 @@ describe MyStoreController do
       # instead of fixtures in 'it' or 'before.each' I can use mock and stub
       get :index
       response.should be_success
+      assert assigns(:my_test_scaffolds) #makes sure that a @my_test_scaffolds instance variable was set
+      assert assigns(:shop_cart) #makes sure that a @shop_cart instance variable was set
     end
   end
 
