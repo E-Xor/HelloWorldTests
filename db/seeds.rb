0d65411e4b6d7404f7bd8d300adbe58da96bc88b
E-Xor <e-xormail@tut.by>
2012-06-08 22:05:51 +0300

    Payment type seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,27 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-MyTestScaffold.delete_all
+#MyTestScaffold.delete_all
+#
+#MyTestScaffold.create(:title => 'Programming Ruby 1.9' ,
+#  :description => %{
+#Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.
+#  },
+#  :image_url => '/images/ruby.jpg' ,
+#  :price => 49.50
+#)
+#
+#MyTestScaffold.create(:title => 'Web Design for Developers' ,
+#  :description => %{
+#Web Design for Developers will show you how to make your web-based application look professionally designed. We'll help you learn how to pick the right colors and fonts, avoid costly interface and accessibility mistakes - your application will really come alive. We'll also walk you through some common Photoshop and CSS techniques and work through a web site redesign, taking a new design from concept all the way to implementation
+#  },
+#  :image_url => '/images/wdfd.jpg' ,
+#  :price => 42.95
+#)
 
-MyTestScaffold.create(:title => 'Programming Ruby 1.9' ,
-  :description => %{
-Ruby is the fastest growing and most exciting dynamic language out there. If you need to get working programs delivered fast, you should add Ruby to your toolbox.
-  },
-  :image_url => '/images/ruby.jpg' ,
-  :price => 49.50
-)
-
-MyTestScaffold.create(:title => 'Web Design for Developers' ,
-  :description => %{
-Web Design for Developers will show you how to make your web-based application look professionally designed. We'll help you learn how to pick the right colors and fonts, avoid costly interface and accessibility mistakes - your application will really come alive. We'll also walk you through some common Photoshop and CSS techniques and work through a web site redesign, taking a new design from concept all the way to implementation
-  },
-  :image_url => '/images/wdfd.jpg' ,
-  :price => 42.95
-)
+PaymentType.delete_all
+PaymentType.create( :payment_type => "Check" )
+PaymentType.create( :payment_type => "Credit/Debet Card" )
+PaymentType.create( :payment_type => "Purchase" )
+PaymentType.create( :payment_type => "PayPal" )
 
