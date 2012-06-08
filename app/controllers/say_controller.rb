1509f09b8149113f3c1ae8ab29f172a1def5fa2f
E-Xor <e-xormail@tut.by>
2012-06-08 21:50:15 +0300

    Cookies experiment

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -4,9 +4,16 @@ class SayController < ApplicationController
     @files = Dir.glob('*')
     @current_file = __FILE__
     @current_dir = File.dirname(__FILE__)
+    cookies[:good_bye] = { :value => "Cookie for goodbye", :expires => 1.hour.from_now }
+    session[:good_bye] = "Session for goodbye"
   end
 
   def goodbye
+    @cookie = ''
+    @cookie = cookies[:good_bye] if cookies[:good_bye]
+    @sess = ''
+    @sess = session[:good_bye] if session[:good_bye]
+
   end
 
 end
